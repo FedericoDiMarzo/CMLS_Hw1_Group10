115ebbdf955724cec93d6d415a215d8196eabb5c
barefootluke <62575399+barefootluke@users.noreply.github.com>
2020-04-22 13:32:12 +0200

    Update lukas.py

--- lukas.py
+++ lukas.py
@@ -15,3 +15,11 @@ ipd.Audio(audio_path)
 plt.figure(figsize=(14, 5))
 librosa.display.waveplot(audio, sr=fs)
 plt.show()
+
+
+def chroma_freq():
+    # Loadign the file
+    x, sr = librosa.load('../simple_piano.wav')hop_length = 512
+    chromagram = librosa.feature.chroma_stft(x, sr=sr, hop_length=hop_length)
+    plt.figure(figsize=(15, 5))
+    librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', hop_length=hop_length, cmap='coolwarm')
