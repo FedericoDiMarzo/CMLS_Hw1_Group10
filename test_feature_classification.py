db447a6898cf41e18406b9b02f36341244d7621c
Aliette7878 <aliette.ravillion@yahoo.fr>
2020-04-30 21:19:00 +0200

    Trying normalization with preprocessing.MinmaxScaler( )

--- test_feature_classification.py
+++ test_feature_classification.py
@@ -18,20 +18,26 @@ dataframe = clean_data(dataframe)
 dataframe = DataNormalizer(dataframe).transform(dataframe)
 
 
-''' OBSOLETE NOW
-# splitting train and test data
-X, y = common.split_Xy(dataframe.values)
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-'''
+
 # selecting the best features
 dataframe = select_features(dataframe)
 dataframe.to_csv(os.path.join(dataframe_folder, 'selected_features.csv'))
 
-# building training and testing dataset
-X_train,X_test,y_train,y_test=get_Xy_train_test()
+'1 - building training and testing set with preprocessing method'
+from sklearn import preprocessing
+X, y = common.split_Xy(dataframe.values)
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+std_scale = preprocessing.MinMaxScaler().fit(X_train) #or replace MinMax by Standard
+X_train_norm = std_scale.transform(X_train)
+X_test_norm = std_scale.transform(X_test)
+
+'''
+2- building training and testing dataset with X-_train_test
+X_train_norm,X_test_norm,y_train,y_test=get_Xy_train_test()
+'''
 
 # defining the classifier
-set_classifier(X_train, y_train, classifier_type='svm')
+set_classifier(X_train_norm, y_train, classifier_type='svm')
 
 # testing the classifier
-test_classifier(X_test, y_test)
+test_classifier(X_test_norm, y_test)
